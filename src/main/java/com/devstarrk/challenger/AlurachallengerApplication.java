888df9f4bc2d7f67bca3adcaf2e3dd0bcb99fc36
MichaelSchimittDev <michaelschimittpro@gmail.com>
2024-01-14 05:11:47 -0300

    project created

---
+++ src/main/java/com/devstarrk/challenger/AlurachallengerApplication.java
@@ -0,0 +1,13 @@
+package com.devstarrk.challenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlurachallengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlurachallengerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devstarrk/challenger/AlurachallengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devstarrk.challenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlurachallengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
