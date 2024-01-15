935f05dd2ff8d034b3e76143791e6aff8f3365e3
MichaelSchimittDev <michaelschimittpro@gmail.com>
2024-01-15 10:09:20 -0300

    /put request created

--- src/main/java/com/devstarrk/challenger/controllers/VideoController.java
+++ src/main/java/com/devstarrk/challenger/controllers/VideoController.java
@@ -2,14 +2,13 @@ package com.devstarrk.challenger.controllers;
 
 import com.devstarrk.challenger.dto.VideoDTO;
 import com.devstarrk.challenger.services.VideoService;
+
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/videos")
@@ -25,5 +24,10 @@ public class VideoController {
     public ResponseEntity<VideoDTO> findById(@PathVariable Long id){
         VideoDTO dto = service.findById(id);
         return ResponseEntity.ok(dto);
+    }
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<VideoDTO> update(@PathVariable Long id,@Valid @RequestBody VideoDTO dto){
+        ResponseEntity<VideoDTO> response = service.update(id, dto);
+        return ResponseEntity.ok(response.getBody());
     }
 }
--- src/main/java/com/devstarrk/challenger/services/VideoService.java
+++ src/main/java/com/devstarrk/challenger/services/VideoService.java
@@ -7,6 +7,7 @@ import com.devstarrk.challenger.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,5 +25,18 @@ public class VideoService {
         Video video = repository.findById(id).orElseThrow(
                 () -> new ResourceNotFoundException("Recurso nao encontrado"));
         return new VideoDTO(video);
+    }
+    @Transactional
+    public ResponseEntity<VideoDTO> update(Long id, VideoDTO dto){
+        Video entity = repository.getReferenceById(id);
+        copyDtoToEntity(dto,entity);
+        entity = repository.save(entity);
+        return ResponseEntity.ok(new VideoDTO(entity));
+    }
+
+    private void copyDtoToEntity(VideoDTO dto, Video entity) {
+        entity.setTitle(dto.getTitle());
+        entity.setDescription(dto.getDescription());
+        entity.setUrl(dto.getUrl());
     }
 }
